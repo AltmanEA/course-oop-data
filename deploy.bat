9e76e5b7f58cd39d83f0513a262bb7f51c0c546a
Евгений Альтман <altmanea@gmail.com>
2022-03-16 17:07:20 +0600

    exam questions update

--- deploy.bat
+++ deploy.bat
@@ -2,7 +2,7 @@ hugo
 
 SET CURDRIVE=%CD:~0,3%
 
-ROBOCOPY ".\public"   "%CURDRIVE%Work\AltmanEA.github.io\1c" /S
+ROBOCOPY ".\public"   "%CURDRIVE%Work\AltmanEA.github.io\oop-data" /S
 
 RD /S /Q ".\public\"
 
